270fd2f57de0ff93905ad85989b5d6d7a8699447
Oc-found <153354935+Oc-found@users.noreply.github.com>
2024-03-08 20:46:31 +0800

    Update runner.py

--- runner.py
+++ runner.py
@@ -1,8 +1,49 @@
 #The starter file.
 #
 # By --
-
+'''
 from words import words as w
 
 
 word = input('> ')
+'''
+
+from words import words as w
+
+def a(w,e):
+    for o in range(len(w)):
+        for i in w:
+            if i in e:
+                e = Nstr1(e)-Nstr1(i)
+    return e
+
+# The str can't to '-'
+#But Nstr1 can!
+class Nstr1:
+    def __init__(self, arg):
+        self.x=arg
+    def __sub__(self,other):
+        c=self.x.replace(other.x,"")
+        return c
+        
+while 1:
+    word = input('> ')
+             
+    print(a(w,word))
+    
+#end
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
